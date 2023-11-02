a9d818317f0a5313cf522f2602f5e1a288c9d3f2
chenzhen <599422271@qq.com>
2023-11-02 09:11:47 +0800

    发布文章

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #!/usr/bin/env sh
-# # 确保脚本抛出遇到的错误
-# set -e
+# 确保脚本抛出遇到的错误
+set -e
 # # 生成静态文件
 # npm run build 
 
@@ -11,34 +11,20 @@ TARGET_BRANCH="develop" # 您要推送到的目标分支
 # 指定要检查的远程仓库名称
 remote_name="https://gitee.com/magicBegin/vuepress-blog.git"
 
-# 进入打包目录
-cd $SOURCE_DIR
-
-# 使用git remote命令列出所有远程仓库
-remotes=$(git remote)
-
-# 检查指定的远程仓库是否存在
-if [[ $remotes == *"$remote_name"* ]]; then
-  echo "远程仓库 $remote_name 存在"
-else
-  # 添加远程仓库地址
-  git remote add origin https://gitee.com/magicBegin/vuepress-blog.git
+# 确保目录存在
+if [ ! -d "$SOURCE_DIR" ]; then
+  echo "Error: 打包目录不存在"
+  exit 1
 fi
-
-# 初始化Git仓库
-git init
 
 # 添加所有文件到Git暂存区
 git add .
-
 # 提交到本地仓库
 git commit -m "发布文章"
-
-# 切换到目标分支
-git checkout  $TARGET_BRANCH
 
 # 推送到远程仓库
 git push origin $TARGET_BRANCH
 
 # 打印操作完成信息
-echo "Pushed to $TARGET_BRANCH successfully"
+# echo "Pushed to $TARGET_BRANCH successfully"
+echo "打包目录已成功提交到 $TARGET_BRANCH 分支"
