0e24022b0281c2ef90965d4792b10017c95e4a06
chenzhen <599422271@qq.com>
2023-11-20 17:15:25 +0800

    变更icon地址

--- assets/pageview-81890077.js
+++
@@ -1,1 +0,0 @@
-import{bQ as e}from"./app-93a09b62.js";const a=()=>e({serverURL:"https://waline.zfhblog.top"});export{a as updatePageview};
---
+++ assets/pageview-979b65e5.js
@@ -0,0 +1,1 @@
+import{bQ as e}from"./app-37ca5a82.js";const a=()=>e({serverURL:"https://waline.zfhblog.top"});export{a as updatePageview};
