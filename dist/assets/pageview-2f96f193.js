57ddd0ddc09fb9a2ab3922e3dcfc9be1684e78d8
chenzhen <599422271@qq.com>
2023-11-22 15:12:07 +0800

    Add package from dist

---
+++ dist/assets/pageview-2f96f193.js
@@ -0,0 +1,1 @@
+import{bQ as e}from"./app-b4188d6d.js";const a=()=>e({serverURL:"https://waline.zfhblog.top"});export{a as updatePageview};
--- dist/assets/pageview-ca071500.js
+++
@@ -1,1 +0,0 @@
-import{bQ as e}from"./app-1bb92488.js";const a=()=>e({serverURL:"https://waline.zfhblog.top"});export{a as updatePageview};
