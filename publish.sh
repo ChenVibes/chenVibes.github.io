1dc8ff6913bdaeeddfa033d4b0c93152a2065dff
chenzhen <599422271@qq.com>
2023-11-22 16:18:48 +0800

    优化打包

--- publish.sh
+++ publish.sh
@@ -10,21 +10,15 @@ target_branch="develop"
 # 设置变量
 source_dir="dist" # VuePress打包后的目录
 
-
-git checkout -b $source_branch
 # 添加所有文件到Git暂存区
-cd $source_dir
-git add .
+# cd $source_dir
+git add ./$source_dir/ -A
 # 提交到本地仓库
 git commit -m "Add package from $source_dir"
-
-# 切换到目标分支
+# 切换到模板分支
 git checkout $target_branch
-
-# 合并源分支的更改到目标分支
-git merge $source_branch
 # 推送到远程仓库
-git push origin $target_branch
+git push -f https://gitee.com/magicBegin/vuepress-blog.git develop
 
 # 打印操作完成信息
 # echo "Pushed to $target_branch successfully"
