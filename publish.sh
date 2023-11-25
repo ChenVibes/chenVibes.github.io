9ed7cfc85847c51933bb738607ae10bcfa6464b3
chenzhen <32085549+frontchen@users.noreply.github.com>
2023-11-25 20:55:58 +0800

    修改发布命令

--- publish.sh
+++ publish.sh
@@ -13,16 +13,20 @@ source_dir="dist" # VuePress打包后的目录
 # 添加所有文件到Git暂存区
 # cd $source_dir
 git add ./$source_dir/ -A -f
+
+git checkout -b stage_2
 # 提交到本地仓库
 git commit -m "Add package from $source_dir"
 
 # 切换到模板分支
 git checkout $target_branch
 
-git merge --strategy-option=theirs $source_branch
+git merge --strategy-option=theirs stage_2
+
 # 推送到远程仓库
 git push -f https://gitee.com/magicBegin/vuepress-blog.git develop
 
+git branch -d stage_2
 # 打印操作完成信息
 # echo "Pushed to $target_branch successfully"
 echo "打包目录已成功提交到 $target_branch 分支"
