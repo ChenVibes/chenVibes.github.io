2f3c11bdce61004cd2d58d1838f14427bb6a89ac
chenzhen <599422271@qq.com>
2023-11-26 18:46:48 +0800

    打包脚本

--- publish.sh
+++ publish.sh
@@ -2,64 +2,31 @@
 #!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
-
-# 检出临时分支
-git checkout -b stage_2
-
-# 定义错误处理函数
-handle_error() {
-  git checkout $source_branch
-  rm -rf dist
-  git branch -d stage_2
- echo "脚本发生错误,已回退 $source_branch 分支。"
-
-}
-
-# 设置错误处理函数
-trap handle_error ERR
-# # 生成静态文件
-npm run build 
-# 设置源分支和目标分支
 source_branch="master"
 target_branch="develop"
 # 设置变量
 source_dir="dist" # VuePress打包后的目录
+# 构建 VuePress
+npm run build
 
-# 添加所有文件到Git暂存区
-# cd $source_dir
-git add ./$source_dir/ -A -f
-# 提交到本地仓库
-git commit -m "Add package from $source_dir"
-
-
-
-# 删除除了gitignore和dist文件夹之外所有文件
-find . ! -path "./.github/*" ! -path "./.git/*" ! -path "./dist/*" ! -path "./node_modules/*" ! -name ".gitignore" ! -name "publish.sh" ! -name "publish.cjs" -type f -delete
-
-# 复制dist文件夹内所有文件 到当前目录
-cp -R dist/* .
-# 添加所有文件到Git暂存区
-find .  -path "./dist/*" -path "./docs/*" -type d -delete
-# cd $source_dir
-git add  -A -f 
-# 提交到本地仓库
-git commit -m "Add package from $source_dir"
-
-# 切换到模板分支 develop
+# 切换到目标分支
 git checkout $target_branch
 
-git merge --strategy-option=theirs stage_2
+# 删除目标分支下的旧文件
+git rm -r .
+
+# 复制构建生成的文件到当前目录
+cp -R dist/* .
+find .  -path "./dist/*" -path "./category/*" -path "./assets/*" -type d -delete
+# 添加所有更改
 git add  -A -f
-# 提交到本地仓库
-git commit -m "Add package from $source_dir"
 
-# 推送到远程仓库
+# 提交更改
+git commit -m "Update VuePress build"
+
+# 推送更改到远程仓库
 git push -f https://gitee.com/magicBegin/vuepress-blog.git develop
-git branch -d stage_2
 
-# 打印操作完成信息
-# echo "Pushed to $target_branch successfully"
 echo "打包目录已成功提交到 $target_branch 分支"
-
-# 切换到代码分支 执行后续自动部署
+# 切换回原分支
 git checkout $source_branch
