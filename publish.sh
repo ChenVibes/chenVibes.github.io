3532f8a8035e123275a2ec8251567124fe2b0ef8
chenzhen <599422271@qq.com>
2023-11-26 11:01:02 +0800

    修复打包

--- publish.sh
+++ publish.sh
@@ -26,7 +26,7 @@ target_branch="develop"
 source_dir="dist" # VuePress打包后的目录
 
 # 删除除了gitignore和dist文件夹之外所有文件
-find . ! -path "./.github/*" ! -path "./.git/*" ! -path "./dist/*" ! -path "./node_modules/*"  -type d -delete
+find . ! -path "./.yarn/*" ! -path "./.npm/*"  ! -path "./.github/*" ! -path "./.git/*" ! -path "./dist/*" ! -path "./node_modules/*"  -type d -delete
 find . ! -name ".gitignore" ! -name "publish.sh" ! -name "publish.cjs" -type f -delete
 # 复制dist文件夹内所有文件 到当前目录
 cp -R dist/* .
