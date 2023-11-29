5a72babe8980d976dec38eed677878af3fdfa5a2
chenzhen <599422271@qq.com>
2023-11-29 09:06:54 +0800

    Add package from dist

---
+++ assets/index.html-28b61fe8.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue_export-helper-c27b6911.js";import{r as t,o as n,c as a,a as c}from"./app-6987be88.js";const r={};function _(s,l){const e=t("AutoCatalog");return n(),a("div",null,[c(e,{index:""})])}const d=o(r,[["render",_],["__file","index.html.vue"]]);export{d as default};
