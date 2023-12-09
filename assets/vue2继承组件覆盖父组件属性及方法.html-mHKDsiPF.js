fe4bb5b5b077fba6b5e9fa574a09ad7d6de428fa
chenzhen <599422271@qq.com>
2023-12-09 16:21:42 +0800

    Add package from dist

---
+++ assets/vue2继承组件覆盖父组件属性及方法.html-mHKDsiPF.js
@@ -0,0 +1,12 @@
+import{_ as n,o as s,c as a,e as t}from"./app-bUbahKpY.js";const e={},p=t(`<p>vue 使用组件继承时 经常遇到方法无法覆盖，这里记录下次避免踩坑</p><div class="language-vue line-numbers-mode" data-ext="vue"><pre class="language-vue"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span><span class="token punctuation">&gt;</span></span><span class="token script"><span class="token language-javascript">
+<span class="token keyword">import</span> Component <span class="token keyword">from</span> <span class="token string">&#39;a&#39;</span>
+<span class="token keyword">export</span> <span class="token keyword">default</span> <span class="token punctuation">{</span>
+  <span class="token keyword">extends</span><span class="token operator">:</span> Component<span class="token punctuation">,</span>
+  <span class="token comment">// 覆盖父组件 mounted方法</span>
+  <span class="token function">beforeCreate</span><span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+    <span class="token keyword">const</span> options <span class="token operator">=</span> <span class="token keyword">this</span><span class="token punctuation">.</span>$options
+    options<span class="token punctuation">.</span>extends<span class="token punctuation">.</span><span class="token function-variable function">mounted</span> <span class="token operator">=</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token punctuation">{</span><span class="token punctuation">}</span>
+  <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">&gt;</span></span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),o=[p];function c(l,i){return s(),a("div",null,o)}const r=n(e,[["render",c],["__file","vue2继承组件覆盖父组件属性及方法.html.vue"]]);export{r as default};
