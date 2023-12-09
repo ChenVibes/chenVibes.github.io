bef6840149ea9151022d9cbc2c3a7205ace209f2
chenzhen <599422271@qq.com>
2023-12-09 14:32:03 +0800

    升级vuepress-theme-hope主题版本为2.0 变更版本管理yarn为pnpm

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,16 +1,12 @@
 import { defineClientConfig } from '@vuepress/client'
-import { defineVuePlaygroundConfig } from 'vuepress-plugin-md-enhance/client'
 import 'element-plus/dist/index.css'
-
-defineVuePlaygroundConfig({
-  // 在此设置 @vue/repl 选项
-})
-
+import globalComponents from './globalComponents'
 export default defineClientConfig({
   async enhance({ app }) {
     if (!__VUEPRESS_SSR__) {
       const ElementPlus = await import('element-plus')
       app.use(ElementPlus)
+      globalComponents(app)
     }
   }
 })
