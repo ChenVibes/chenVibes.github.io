54808ae6d92678709aca30eda342aec0541ead74
chenzhen <599422271@qq.com>
2024-01-04 20:20:54 +0800

    Add package from dist

---
+++ assets/DemoVirtualScrollListItem-ZKsECHsS.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c,t}from"./app-ZyR8DK8y.js";const l={__name:"DemoVirtualScrollListItem",props:{source:Object,uniqueKey:[Number,String]},setup(o){const e=o;return(u,a)=>(s(),c("p",{key:e.uniqueKey},t(e.source.value)+" "+t(e.source.label),1))}},n=r(l,[["__file","DemoVirtualScrollListItem.vue"]]);export{n as default};
